13db5faedbdf216c242de1075b1959437e290afa
ASXFA <fachri.agustian12@gmail.com>
2021-08-29 09:25:55 +0700

    update v3

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -78,6 +78,70 @@ class Auth extends CI_Controller
         }
     }
 
+    public function action_login_android()
+    {
+        $this->load->model('model_akun');
+        $email = $this->input->post('email');
+        $cek = $this->model_akun->getBy(array('email_akun' => $email));
+        $pesan = array();
+        if ($cek->num_rows() > 0) {
+            $akun = $cek->row();
+            $pass = $this->input->post('password');
+            if ($akun->level_akun == 1) {
+                if (password_verify($pass, $akun->password_akun)) {
+                    $session = array(
+                        'isLogin' => 1,
+                        'id_akun_login' => $akun->id_akun,
+                        'id_user_akun_login' => $akun->id_user_akun,
+                        'email_akun_login' => $akun->email_akun,
+                        'level_akun_login' => $akun->level_akun
+                    );
+                    $this->session->set_userdata($session);
+                    $pesan['isLogin'] = 1;
+                    $pesan['id_user'] = $akun->id_user_akun;
+                    $pesan['condition'] = 2;
+                    $pesan['pesan'] = "Login Berhasil !";
+                    $pesan['url'] = 'dashboard';
+                    echo json_encode($pesan);
+                    // if ($role == 1) {
+                    //     // $this->session->set_flashdata('msgLogin','Halo, Selamat Datang ');
+                    //     $pesan['condition'] = 2;
+                    //     $pesan['pesan'] = "Login Berhasil !";
+                    //     $pesan['url'] = 'semuaAnggaranKegiatan';
+                    //     echo json_encode($pesan);
+                    // }else if($role == 2){
+                    //     // $this->session->set_flashdata('msgLogin','Halo, Selamat Datang ');
+                    //     $pesan['condition'] = 2;
+                    //     $pesan['pesan'] = "Login Berhasil !";
+                    //     $pesan['url'] = 'semuaAnggaranKegiatan';
+                    //     echo json_encode($pesan);
+                    // }else if($role == 3){
+                    //     // $this->session->set_flashdata('msgLogin','Halo, Selamat Datang ');
+                    //     $pesan['condition'] = 2;
+                    //     $pesan['pesan'] = "Login Berhasil !";
+                    //     $pesan['url'] = 'semuaAnggaranKegiatan';
+                    //     echo json_encode($pesan);
+                    // }
+                } else {
+                    // $this->session->set_flashdata('msgLogin','Password tidak cocok !');
+                    $pesan['condition'] = 1;
+                    $pesan['pesan'] = "Login Gagal ! Password Tidak Cocok";
+                    echo json_encode($pesan);
+                }
+            } else {
+                $pesan['condition'] = 0;
+                $pesan['pesan'] = "Hanya diperuntukkan untuk bagian Akademik ";
+
+                echo json_encode($pesan);
+            }
+        } else {
+            // $this->session->set_flashdata('msgLogin','NIP Tidak terdaftar !');
+            $pesan['condition'] = 0;
+            $pesan['pesan'] = "Login Gagal ! Email Tidak tersedia !";
+            echo json_encode($pesan);
+        }
+    }
+
     public function logoutAndroid()
     {
         $isLogin = $this->input->post('isLogin');
